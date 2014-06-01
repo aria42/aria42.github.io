703287b592eca8d8f56af8a6c3b773c894ed0b3a
ahaghighi <user.email=ahaghighi@apple.com>
2014-05-31 20:34:32 -0700

    svg working

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,3 +1,30 @@
 $(".full img").on("click", function() {
   $(this).toggleClass("zoom");
 });
+
+(function() {
+
+  $(function() {
+    var $targets;
+    $targets = $('img[src$="svg"]').hide();
+    return $targets.each(function(i, item) {
+      var _this = this;
+      return $.get(this.src).success(function(data) {
+        var $svg, a, nName, nValue, _attr, _i, _len;
+        $svg = $(data).find('svg');
+        _attr = _this.attributes;
+        $.extend(_attr, $svg[0].attributes);
+        for (_i = 0, _len = _attr.length; _i < _len; _i++) {
+          a = _attr[_i];
+          nName = a.nodeName;
+          nValue = a.nodeValue;
+          if (nName !== 'src' && nName !== 'style') {
+            $svg.attr(nName, nValue);
+          }
+        }
+        return $(_this).replaceWith($svg);
+      });
+    });
+  });
+
+}).call(this);
